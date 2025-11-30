c7939b9977a63f482bf194b21c3760c0f33ceebf
vivek gangadharan <vgangadharan@ilionx.com>
2025-11-30 12:26:28 +0100

    account base

--- database/database.go
+++ database/database.go
@@ -3,6 +3,8 @@ package database
 import (
 	"hippo-bpm/models"
 	"log"
+	"os"
+	"path/filepath"
 
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
@@ -11,16 +13,32 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-	db, err := gorm.Open(sqlite.Open("accounts.db"), &gorm.Config{})
+	// Get user home directory
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal("Cannot get user home directory:", err)
+	}
+
+	// Create an app-specific folder
+	appDir := filepath.Join(homeDir, ".account_explorer")
+	if err := os.MkdirAll(appDir, os.ModePerm); err != nil {
+		log.Fatal("Cannot create app directory:", err)
+	}
+
+	// Database file path
+	dbPath := filepath.Join(appDir, "accounts.db")
+
+	// Connect to SQLite
+	db, err := gorm.Open(sqlite.Open(dbPath), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect database:", err)
 	}
 
-	// Auto migrate tables
-	err = db.AutoMigrate(&models.Account{})
-	if err != nil {
+	// Auto-migrate tables
+	if err := db.AutoMigrate(&models.Account{}); err != nil {
 		log.Fatal("Failed to migrate database:", err)
 	}
 
 	DB = db
+	log.Println("Database created at:", dbPath)
 }
